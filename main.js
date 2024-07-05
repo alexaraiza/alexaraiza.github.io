4cc289d4e6d723f02d22fcd50a6871f7f2318284
Alex Araiza <alexandre.araiza@gmail.com>
2024-07-05 10:00:00 +0000

    v2

--- main.js
+++ main.js
@@ -1,99 +1,78 @@
-window.addEventListener("load", loadPage);
-copyEmailButton.addEventListener("click", copyEmail);
-copyPhoneNumberButton.addEventListener("click", copyPhoneNumber);
-copyLinkedInButton.addEventListener("click", copyLinkedIn);
-contactForm.addEventListener("submit", submitContactForm);
+window.addEventListener('load', () => (document.body.style.opacity = ''));
+document.getElementById('copyEmailButton').addEventListener('click', copyEmail);
+document.getElementById('copyLinkedinURLButton').addEventListener('click', copyLinkedIn);
 
+const contactForm = document.getElementById('contactForm');
+const contactFormSubmitButton = document.getElementById('contactFormSubmitButton');
+contactForm.addEventListener('submit', submitContactForm);
 
-function loadPage() {
-  const PROFESSION_TEXT = profession.innerHTML;
-  profession.style.height = getComputedStyle(profession).height;
-  profession.innerHTML = "";
-  myProjectsButton.style.visibility = "hidden";
-  myProjectsButton.style.opacity = 0;
+const intersectionObserver = new IntersectionObserver(
+  (entries) => {
+    for (const entry of entries) {
+      if (entry.isIntersecting) {
+        document.querySelector(`a[href="#${entry.target.id}"]`).classList.add('active');
+      } else {
+        document.querySelector(`a[href="#${entry.target.id}"]`).classList.remove('active');
+      }
+    }
+  },
+  {
+    root: document.querySelector('.projects'),
+    threshold: 0.5,
+  }
+);
 
-  document.body.style.visibility = "";
-  document.body.style.opacity = "";
-
-  setTimeout(() => typeOut(PROFESSION_TEXT, profession), 1000);
-  setTimeout(function() {
-    myProjectsButton.style.visibility = "";
-    myProjectsButton.style.opacity = "";
-  }, 3600);
+for (const project of document.querySelectorAll('.project')) {
+  intersectionObserver.observe(project);
 }
 
+for (const paginationLink of document.querySelectorAll('.pagination > a')) {
+  paginationLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    document.querySelector(paginationLink.getAttribute('href')).scrollIntoView({ block: 'nearest' });
+  });
+}
 
 function copyEmail() {
-  email.disabled = false;
-  email.select();
-  document.execCommand("copy");
-  email.disabled = true;
-}
-
-function copyPhoneNumber() {
-  phoneNumber.disabled = false;
-  const PHONE_TEXT = phoneNumber.value;
-  phoneNumber.value = PHONE_TEXT.replaceAll(" ", "");
-  phoneNumber.select();
-  document.execCommand("copy");
-  phoneNumber.value = PHONE_TEXT;
-  phoneNumber.disabled = true;
+  navigator.clipboard.writeText(document.getElementById('email').innerHTML);
 }
 
 function copyLinkedIn() {
-  linkedin.disabled = false;
-  linkedin.select();
-  document.execCommand("copy");
-  linkedin.disabled = true;
+  navigator.clipboard.writeText(document.getElementById('linkedinURL').innerHTML);
 }
-
-
-function typeOut(text, element) {
-  let textEndIndex = 0;
-  let typeCharacterInterval = setInterval(function() {
-    element.innerHTML = text.substring(0, ++textEndIndex);
-    if (textEndIndex === text.length) {
-      clearInterval(typeCharacterInterval);
-    }
-  }, 80);
-}
-
 
 function submitContactForm(event) {
   contactFormSubmitButton.disabled = true;
-  formFeedback.innerHTML = "";
+  formFeedback.innerHTML = '';
   event.preventDefault();
 
-  const contactFormData = {
-    name: contactFormName.value,
-    email: contactFormEmail.value,
-    message: contactFormMessage.value
-  };
+  const datetime = new Date();
+  const contactFormData = Object.fromEntries(new FormData(contactForm));
+  contactFormData.isoTimestamp = datetime.toISOString();
+  contactFormData.timestamp = datetime.toString();
 
-  fetch("https://alexaraiza-contact-default-rtdb.firebaseio.com/contact.json",
-  {
-    method: "POST",
+  fetch('https://alexaraiza-contact-default-rtdb.firebaseio.com/contact.json', {
+    method: 'POST',
     body: JSON.stringify(contactFormData),
     headers: {
-      "Content-Type": "application/json"
-    }
+      'Content-Type': 'application/json',
+    },
   })
-  .then((response) => {
-    if (response.ok) {
-      contactForm.reset();
-      formFeedback.setAttribute("class", "success");
-      formFeedback.innerHTML = "Thank you for your message!";
-    }
-    else {
-      formFeedback.setAttribute("class", "error");
-      formFeedback.innerHTML = "Something went wrong. Please try again.";
-    }
-  })
-  .catch(() => {
-    formFeedback.setAttribute("class", "error");
-    formFeedback.innerHTML = "Something went wrong. Please check your network connection and try again.";
-  })
-  .finally(() => {
-    contactFormSubmitButton.disabled = false;
-  });
+    .then((response) => {
+      if (response.ok) {
+        contactForm.reset();
+        formFeedback.setAttribute('class', 'success');
+        formFeedback.innerHTML = 'Thank you for your message!';
+      } else {
+        formFeedback.setAttribute('class', 'error');
+        formFeedback.innerHTML = 'Something went wrong. Please try again.';
+      }
+    })
+    .catch(() => {
+      formFeedback.setAttribute('class', 'error');
+      formFeedback.innerHTML = 'Something went wrong. Please check your network connection and try again.';
+    })
+    .finally(() => {
+      contactFormSubmitButton.disabled = false;
+    });
 }
